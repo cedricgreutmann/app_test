8bb41f8819fa6e93526fbdde1408ba5262f113bc
Cedric Greutmann <cedric.greutmann@vodafone.com>
2020-05-20 13:32:14 +0100

    Updates

---
+++ precache-manifest.a762f1f3c4a7a6f8da22f51e93487a4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80595a30c42be6a22cca5e72fff97d79",
+    "url": "/app_test/index.html"
+  },
+  {
+    "revision": "6637b12a88d7fee63ce8",
+    "url": "/app_test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b7644a2e8a31d2064850",
+    "url": "/app_test/static/js/2.a9377df8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app_test/static/js/2.a9377df8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6637b12a88d7fee63ce8",
+    "url": "/app_test/static/js/main.7b109942.chunk.js"
+  },
+  {
+    "revision": "2b084238c6dfd509c3d3",
+    "url": "/app_test/static/js/runtime-main.c234a44c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app_test/static/media/logo.5d5d9eef.svg"
+  }
+]);
